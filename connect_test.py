00f031da93ed66a1244f2a50b73903727b88051c
Mike Rosengrant <mike.rosengrant@nanome.ai>
2024-11-01 11:44:50 -0400

    Update photon_setup, add requirements/ connect test script

---
+++ connect_test.py
@@ -0,0 +1,22 @@
+import os
+import winrm
+import yaml
+
+inventory_file = os.path.join(os.path.dirname(__file__), 'inventory.local.yaml')
+inventory = yaml.safe_load(open(inventory_file))
+windows_host = inventory['myhosts']['hosts']['windows']
+
+username = windows_host['ansible_user']
+pwd = windows_host['ansible_password']
+host = windows_host['ansible_host']
+
+session = winrm.Session(
+    host,
+    auth=(username, pwd),
+    transport='ntlm',
+    server_cert_validation='ignore',
+)
+if not session:
+    raise Exception('Cannot connect to Windows host')
+else:
+    print('Connected to Windows host')
--- photon_setup.py
+++ photon_setup.py
@@ -1,6 +1,14 @@
 import os
 import subprocess
 
+'''
+On MacOS, If you receive this error:
+objc[9034]: +[NSString initialize] may have been in progress in another thread when fork() was called. We cannot safely call it or ignore it in the fork() child process. Crashing instead. Set a breakpoint on objc_initializeAfterForkError to debug.
+ERROR! A worker was found in a dead state
+
+You can try to set the following environment variable:
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+'''
 
 PLAYBOOKS_DIR = os.path.join(os.path.dirname(__file__), 'playbooks')
 inventory_filepath = os.path.join(os.path.dirname(__file__), 'inventory.local.yaml')
