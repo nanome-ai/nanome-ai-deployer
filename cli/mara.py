dfc9a74266f060cbe8da40a996f2990b02209e3c
Mike Rosengrant <mike.rosengrant@nanome.ai>
2025-01-17 16:14:07 -0500

    better utilize azure values already set in .env

--- cli/mara.py
+++ cli/mara.py
@@ -29,24 +29,81 @@ def configure_tool_server(existing_env=None) -> dict:
     return env
 
 
-def configure_azure_envvars() -> dict:
-    llm_url = input("Enter your Azure LLM API URL (e.g https://acme.azure.com/v1): ")
-    embedding_deployment = input('Enter the deployment name for your embedding model (default "text-embedding-ada-002")') or 'text-embedding-ada-002'
-    gpt4_deployment = input('Enter the deployment name for your gpt-4 model (default "gpt-4")') or 'gpt-4'
-    gpt4o_deployment = input('Enter the deployment name for your gpt-4o model (default "gpt-4o")') or 'gpt-4o'
-    gpt3_5_deployment = input('Enter the deployment name for your gpt-3.5 model (default "gpt-3.5")') or 'gpt-3.5'
-    # Collect api versions
-    azure_llm_api_version = input('Enter the API version for your LLM (default "2024-08-01-preview")') or '2024-08-01-preview'
-    azure_embedding_api_version = input('Enter the API version for your embedding model (default "2023-05-15")') or '2023-05-15'
+def configure_azure_envvars(mara_env) -> dict:
+
+    llm_api_url = current_llm_api_url = mara_env.get('LLM_API_URL', None)
+    embedding_deployment = current_embedding_deployment = mara_env.get('EMBEDDING_MODEL', None)
+    gpt4_deployment = current_gpt4_deployment = mara_env.get('LLM_MODEL', None)
+    gpt4o_deployment = current_gpt4o_deployment = mara_env.get('LLM_MODEL_MD', None)
+    gpt35_deployment = current_gpt35_deployment = mara_env.get('LLM_MODEL_SM', None)
+    azure_llm_api_version = current_azure_llm_api_version = mara_env.get('AZURE_LLM_API_VERSION', None)
+    azure_embedding_api_version = current_azure_embedding_api_version = mara_env.get('AZURE_EMBEDDING_API_VERSION', None)
+
+    # Collect Azure LLM API URL
+    update_value = True
+    if current_llm_api_url:
+        modify_answer = input(f'Your current LLM_API_URL is set to {current_llm_api_url}. Press ENTER to continue, or "1" to modify: ')
+        update_value = modify_answer is not ''
+    if update_value:
+        llm_api_url = input("Enter your Azure LLM API URL (e.g https://acme.azure.com/v1): ")
+
+    # Collect Azure EMBEDDING_DEPLOYMENT NAME
+    update_value = True
+    if current_embedding_deployment:
+        modify_answer = input(f'Your current EMBEDDING_DEPLOYMENT is set to {current_embedding_deployment}. Press ENTER to continue, or "1" to modify: ')
+        update_value = modify_answer is not ''
+    if update_value:
+        embedding_deployment = input('Enter the deployment name for your embedding model (default "text-embedding-ada-002") ') or 'text-embedding-ada-002'
+
+    # Collect Azure GPT-4 Deployment name
+    update_value = True
+    if current_gpt4_deployment:
+        modify_answer = input(f'Your current gpt-4 deployment is set to {current_gpt4_deployment}. Press ENTER to continue, or "1" to modify: ')
+        update_value = modify_answer is not ''
+    if update_value:
+        gpt4_deployment = input('Enter the deployment name for your gpt-4 model (default "gpt-4") ') or 'gpt-4'
+
+    # Collect Azure GPT-4o Deployment name
+    update_value = True
+    if current_gpt4o_deployment:
+        modify_answer = input(f'Your current gpt4o_deployment is set to {current_gpt4o_deployment}. Press ENTER to continue, or "1" to modify: ')
+        update_value = modify_answer is not ''
+    if update_value:
+        gpt4o_deployment = input('Enter the deployment name for your gpt-4o model (default "gpt-4o") ') or 'gpt-4o'
+
+    # Collect Azure GPT-3.5 Deployment name
+    update_value = True
+    if current_gpt35_deployment:
+        modify_answer = input(f'Your current gpt35_deployment is set to {current_gpt35_deployment}. Press ENTER to continue, or "1" to modify: ')
+        update_value = modify_answer is not ''
+    if update_value:
+        gpt35_deployment = input('Enter the deployment name for your gpt-3.5 model (default "gpt-3.5") ') or 'gpt-3.5'
+    
+    # Collect llm api version
+    update_value = True
+    if current_azure_llm_api_version:
+        modify_answer = input(f'Your current AZURE_EMBEDDING_API_VERSION is set to {current_azure_llm_api_version}. Press ENTER to continue, or "1" to modify: ')
+        update_value = modify_answer is not ''
+    if update_value:
+        azure_llm_api_version = input('Enter the API version for your LLM (default "2024-08-01-preview") ') or '2024-08-01-preview'
+
+    # Collect embedding api version
+    update_value = True
+    if current_azure_embedding_api_version:
+        modify_answer = input(f'Your current AZURE_EMBEDDING_API_VERSION is set to {current_azure_embedding_api_version}. Press ENTER to continue, or "1" to modify: ')
+        update_value = modify_answer is not ''
+    if update_value:
+        azure_embedding_api_version = input('Enter the API version for your embedding model (default "2023-05-15") ') or '2023-05-15'
+
     env = {
         'OPENAI_API_TYPE': 'azure',
-        'LLM_API_URL': llm_url,
+        'LLM_API_URL': llm_api_url,
         'AZURE_EMBEDDING_API_VERSION': azure_embedding_api_version,
         'AZURE_LLM_API_VERSION': azure_llm_api_version,
         'EMBEDDING_MODEL': embedding_deployment,
         'LLM_MODEL': gpt4_deployment,
         'LLM_MODEL_MD': gpt4o_deployment,
-        'LLM_MODEL_SM': gpt3_5_deployment,
+        'LLM_MODEL_SM': gpt35_deployment,
     }
     return env
 
@@ -55,13 +112,13 @@ def configure_mara_server(existing_mara_env) -> dict:
     env = {}
     while azure_provider not in ['1', '2']:
         azure_provider = input((
-            'Will you be using a Microsoft Azure-hosted LLM such as GPT4?\n'
+            'Will you be using Microsoft Azure-hosted LLMs?\n'
             '1. Yes\n'
             '2. No\n\n'
             'Make a selection (1|2): '
         ))
     if azure_provider == '1':
-        azure_env_vars = configure_azure_envvars()
+        azure_env_vars = configure_azure_envvars(existing_mara_env)
         env.update(azure_env_vars)
 
     exisitng_llm_key = existing_mara_env.get('LLM_API_KEY', None)
