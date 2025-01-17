8db81ef72531cd0e64f22144c14c76d4b93d075a
Mike Rosengrant <mike.rosengrant@nanome.ai>
2025-01-17 15:42:31 -0500

    add more inputs to the azure subroutine

--- cli/mara.py
+++ cli/mara.py
@@ -31,9 +31,22 @@ def configure_tool_server(existing_env=None) -> dict:
 
 def configure_azure_envvars() -> dict:
     llm_url = input("Enter your Azure LLM API URL (e.g https://acme.azure.com/v1): ")
+    embedding_deployment = input('Enter the deployment name for your embedding model (default "text-embedding-ada-002")') or 'text-embedding-ada-002'
+    gpt4_deployment = input('Enter the deployment name for your gpt-4 model (default "gpt-4")') or 'gpt-4'
+    gpt4o_deployment = input('Enter the deployment name for your gpt-4o model (default "gpt-4o")') or 'gpt-4o'
+    gpt3_5_deployment = input('Enter the deployment name for your gpt-3.5 model (default "gpt-3.5")') or 'gpt-3.5'
+    # Collect api versions
+    azure_llm_api_version = input('Enter the API version for your LLM (default "2024-08-01-preview")') or '2024-08-01-preview'
+    azure_embedding_api_version = input('Enter the API version for your embedding model (default "2023-05-15")') or '2023-05-15'
     env = {
         'OPENAI_API_TYPE': 'azure',
         'LLM_API_URL': llm_url,
+        'AZURE_EMBEDDING_API_VERSION': azure_embedding_api_version,
+        'AZURE_LLM_API_VERSION': azure_llm_api_version,
+        'EMBEDDING_MODEL': embedding_deployment,
+        'LLM_MODEL': gpt4_deployment,
+        'LLM_MODEL_MD': gpt4o_deployment,
+        'LLM_MODEL_SM': gpt3_5_deployment,
     }
     return env
 
